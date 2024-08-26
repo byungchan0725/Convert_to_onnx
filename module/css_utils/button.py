28b5b079b489c8bce0052c2b5a8ff4f4de052546
kim byung chan <bgk@vazilcompany.com>
2024-08-26 10:47:45 +0900

    ADD :: Streamlit Button css

---
+++ module/css_utils/button.py
@@ -0,0 +1,12 @@
+def button(): 
+    return """
+    <style>
+    button {
+        width: 100% !important;
+        height: auto !important;
+        padding-top: 10px !important;
+        padding-bottom: 10px !important;
+        box-sizing: border-box; /* Ensure padding is included in the width */
+    }
+    </style>
+    """
